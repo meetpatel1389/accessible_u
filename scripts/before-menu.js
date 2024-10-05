c0314d6aa06168406eac28982b89ea947ab4104a
meetpatel1389 <106921263+meetpatel1389@users.noreply.github.com>
2024-10-04 20:13:35 -0400

    Update before-menu.js
    Signed-off-by: meetpatel1389 <106921263+meetpatel1389@users.noreply.github.com>

--- scripts/before-menu.js
+++ scripts/before-menu.js
@@ -37,3 +37,29 @@
 //     }
 //   }
 // });
+$(window).on("load resize", function() {
+
+  const $dropdown = $(".dropdown");
+  const $dropdownToggle = $(".dropdown-toggle");
+  const $dropdownMenu = $(".dropdown-menu");
+  const showClass = "show";
+
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
